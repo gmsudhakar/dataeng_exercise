b784e02aa9237ec6b19da72d84d905f701622170
gmsudhakar <gmsudhakar@gmail.com>
2022-05-07 22:35:35 +0200

    Update setup.py

--- main/tst/dev/exercise_2/sudhakar/setup.py
+++ main/tst/dev/exercise_2/sudhakar/setup.py
@@ -1,1 +1,11 @@
 
+import setuptools
+
+setuptools.setup(
+    name="example-package",
+    version="1.0.0",
+    author="Sud",
+    author_email="test@test.com",
+    description="Example package",
+    packages=["example-package"]
+)
